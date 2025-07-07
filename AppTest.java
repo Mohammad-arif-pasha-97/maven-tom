4ce55f12a65cb7f147fc53e8fb3c368a470b8a26
Mohammad Arif Pasha <154226365+Mohammad-arif-pasha-97@users.noreply.github.com>
2025-07-07 15:48:36 +0530

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,29 @@
+ 
+
+#If you're using JUnit 5, you need to change your AppTest.java to use the appropriate annotations and classes. 
+
+package com.example; 
+
+ 
+
+import org.junit.jupiter.api.Test; 
+
+import static org.junit.jupiter.api.Assertions.assertTrue; 
+
+ 
+
+public class AppTest { 
+
+ 
+
+    @Test 
+
+    void testExample() { 
+
+        assertTrue(true); // Example test 
+
+    } 
+
+} 
+
+ 
